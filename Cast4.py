ee85918706cc357b2a001f1de6e67c0eec993668
Direccc <112394828+Direccc@users.noreply.github.com>
2024-10-24 03:16:48 +0800

    Add files via upload

---
+++ Cast4.py
@@ -0,0 +1,32 @@
+#Coding Exercise 4.4
+x = 332
+y = 2031
+
+if x >= y:
+    print("x is greater than or equal to y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("x is less than y")
+
+print("")
+#Ending of Coding Exercise 3.4
+
+#Coding Exercise 4.8
+furniture = ["table", "chair", "cabinet", "desk", "couch"]
+
+for x in furniture:
+    if x == "cabinet":
+        continue
+    print(x)
+
+i  = 1
+while i < 15:
+    print(i)
+    i+=1
+else:
+    print("i is no longer less than 15")
+
+print("")
+#Ending of Coding Exercise 3.4
+
